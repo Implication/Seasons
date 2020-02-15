f5c68973c494b921d3d608ebe6af6784ab9192cf
Implication <IamTrajon@gmail.com>
2020-02-14 21:12:29 -0800

    Updates

---
+++ precache-manifest.9715db68ec5c46bcdeb5d141bf13ca57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7447e06d292459d76013fe2a9897e25d",
+    "url": "/Seasons.git/index.html"
+  },
+  {
+    "revision": "13b7ef74fbb8b089006c",
+    "url": "/Seasons.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4026f89668c3edca6f26",
+    "url": "/Seasons.git/static/js/2.c3dbecdc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Seasons.git/static/js/2.c3dbecdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b7ef74fbb8b089006c",
+    "url": "/Seasons.git/static/js/main.f59d3ebd.chunk.js"
+  },
+  {
+    "revision": "ae9c33930f8ec421151f",
+    "url": "/Seasons.git/static/js/runtime-main.02f6fdee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Seasons.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
